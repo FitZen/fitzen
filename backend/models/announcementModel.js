2e253f17fb5cd55714ae6337a8a7953058ccf5e1
anjanasilva99 <108554413+anjanasilva99@users.noreply.github.com>
2023-08-05 13:05:34 +0530

    Add  "Addannouncement" feature

--- backend/models/announcementModel.js
+++ backend/models/announcementModel.js
@@ -1,6 +1,8 @@
 import {query} from '../config/db.js';
 import asyncHandler from 'express-async-handler';
 
+
+// get all announcements
 const getAnnouncements = asyncHandler(async () => {
     const sql = 'SELECT * FROM announcement ORDER BY posted_on DESC;';
     const result = await query(sql);
@@ -8,6 +10,16 @@ const getAnnouncements = asyncHandler(async () => {
     return result.rows;
 });
 
+
+//add announcement
+const addAnnouncement = asyncHandler(async (announcement) => {
+    const sql = 'INSERT INTO announcement (title, content, posted_by) VALUES ($1, $2, $3) RETURNING id';
+    const result = await query(sql, [announcement.title, announcement.content, announcement.posted_by]);
+
+    return result.rows[0];
+});
+
 export {
     getAnnouncements,
+    addAnnouncement,
 };
--- backend/routes/announcementRoutes.js
+++ backend/routes/announcementRoutes.js
@@ -2,10 +2,12 @@ import express from "express";
 const router = express.Router();
 import {
     getAllAnnouncements,
+    addNewAnnouncement,
 } from "../controllers/announcementController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 
 router.get("/announcement",protect, getAllAnnouncements);
+router.post("/announcement",protect, addNewAnnouncement);
 
 export default router;
