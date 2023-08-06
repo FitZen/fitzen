7ff68f21613d42acfb91860dc38ea192306e6e71
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-08-06 21:33:50 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,6 +8,11 @@ const findUserById = asyncHandler(async (id) => {
     const sql = 'SELECT * FROM users WHERE id = $1';
     const result = await query(sql, [id]);
 
+    /*
+    if (result.rows.length === 0) {
+        throw new Error('No user found with this ID');
+    }*/
+
     return result.rows[0];
 });
 
@@ -16,6 +21,11 @@ const findUserById = asyncHandler(async (id) => {
 const findUserByEmail = asyncHandler(async (email) => {
     const sql = 'SELECT * FROM users WHERE email = $1';
     const result = await query(sql, [email]);
+
+    /*
+    if (result.rows.length === 0) {
+        throw new Error('No user found with this email');
+    }*/
 
     return result.rows[0];
 });
@@ -26,6 +36,18 @@ const matchPassword = asyncHandler(async (email, password) => {
     const sql = 'SELECT password FROM users WHERE email = $1';
     const result = await query(sql, [email]);
 
+    /*
+    if (result.rows.length === 0) {
+        throw new Error('No user found with this email');
+    }
+
+    const match = await bcrypt.compare(password, result.rows[0].password);
+    if (!match) {
+        throw new Error('Password does not match');
+    }
+
+    return match; */
+
     return await bcrypt.compare(password, result.rows[0].password);
 });
 
@@ -35,6 +57,14 @@ const setLoginDateTime = asyncHandler(async (id) => {
     const sql = 'UPDATE users SET last_login = $1 WHERE id = $2';
     const currTime = new Date().toISOString();
 
+    /*const result = await query(sql, [currTime, id]);
+
+    if (result.rowCount === 0) {
+        throw new Error('No user found with this ID');
+    }
+
+    return result.rowCount > 0;*/
+
     return await query(sql, [currTime, id]);
 });
 
@@ -42,6 +72,15 @@ const setLoginDateTime = asyncHandler(async (id) => {
 // set login status
 const setLoginStatus = asyncHandler(async (id, status) => {
     const sql = 'UPDATE users SET status = $1 WHERE id = $2';
+
+    /*
+    const result = await query(sql, [status, id]);
+
+    if (result.rowCount === 0) {
+        throw new Error('No user found with this ID');
+    }
+
+    return result.rowCount > 0;*/
 
     return await query(sql, [status, id]);
 });
