8e3a82a7574344ea356d6d925a968912eef4a375
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-08-09 12:07:08 +0530

    Update sendEmail.js
    Setup the sendEmail function, but have to test

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,28 +5,30 @@ import nodemailer from 'nodemailer';
 
 // set up nodemailer transporter
 const transporter = nodemailer.createTransport({
-    host: '',
-    port: '',
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
     secure: true,  // Ensure this is true if you are using port 465, otherwise false
     auth: {
-        user: '',
-        pass: ''
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD
     }
 });
 
 
 /**
  * Send an email.
+ * @param {string} to - The sender's email address.
  * @param {string} to - The recipient's email address.
  * @param {string} subject - The email subject.
  * @param {string} text - The plain text email body.
  * @param {string} html - The HTML email body.
  */
+
 // mail sender
 const sendEmail = async (to, subject, text, html) => {
     try {
         const info = await transporter.sendMail({
-            from: '"Fred Foo 👻" <foo@example.com>', // sender address
+            from: process.env.MAIL_USER,
             to: to,
             subject: subject,
             text: text,
@@ -35,11 +37,13 @@ const sendEmail = async (to, subject, text, html) => {
 
         console.log("Message sent: %s", info.messageId);
     } catch (error) {
-        console.error("Error sending email:", error);
+        console.error("Error sending email: ", error);
     }
 };
 
-// Example Usage: (You can comment this out later)
-sendEmail("bar@example.com", "Hello ✔", "Hello world?", "<b>Hello world?</b>");
+
+// Example Usage:
+// sendEmail("bar@example.com", "Hello ✔", "Hello world?", "<b>Hello world?</b>");
+
 
 export default sendEmail;
