2cfef1aa27a48f907a64ca1708eb8f6bcfdd81b3
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-08-09 11:26:07 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,31 +1,45 @@
 import nodemailer from 'nodemailer';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
+// set up nodemailer transporter
+const transporter = nodemailer.createTransport({
+    host: '',
+    port: '',
+    secure: true,  // Ensure this is true if you are using port 465, otherwise false
     auth: {
-        user: 'your-email@gmail.com',
-        pass: 'your-password'
+        user: '',
+        pass: ''
     }
 });
 
 
-const sendEmail = async (to, subject, text) => {
-    let mailOptions = {
-        from: 'your-email@gmail.com',
-        to: to,
-        subject: subject,
-        text: text
-    };
+/**
+ * Send an email.
+ * @param {string} to - The recipient's email address.
+ * @param {string} subject - The email subject.
+ * @param {string} text - The plain text email body.
+ * @param {string} html - The HTML email body.
+ */
+// mail sender
+const sendEmail = async (to, subject, text, html) => {
+    try {
+        const info = await transporter.sendMail({
+            from: '"Fred Foo 👻" <foo@example.com>', // sender address
+            to: to,
+            subject: subject,
+            text: text,
+            html: html
+        });
 
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            console.error('Error sending email: ', err);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-}
+        console.log("Message sent: %s", info.messageId);
+    } catch (error) {
+        console.error("Error sending email:", error);
+    }
+};
 
+// Example Usage: (You can comment this out later)
+sendEmail("bar@example.com", "Hello ✔", "Hello world?", "<b>Hello world?</b>");
 
 export default sendEmail;
