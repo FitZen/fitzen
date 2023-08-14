ae13f33f4c2747cd7b10450925f8e37bcabbcaa3
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-08-14 17:12:45 +0530

    Create hashPassword.js

---
+++ backend/utils/hashPassword.js
@@ -0,0 +1,11 @@
+import bcrypt from 'bcryptjs';
+
+
+// hash password
+const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+};
+
+
+export default hashPassword;
