f2b3c5f9cd9f60f09ad2ad669a6b98ebc1d4f1bf
Dula0133 <92773152+Dula0133@users.noreply.github.com>
2023-08-15 11:20:30 +0530

    Receptionist Profile Update

--- frontend/src/pages/receiptionist/Profile.jsx
+++ frontend/src/pages/receiptionist/Profile.jsx
@@ -4,84 +4,61 @@ import { Typography } from "@mui/material";
 import {FaTelegram, FaFeatherAlt} from 'react-icons/fa';
 import ProfileImg from '../../assets/photo-1633332755192-727a05c4013d.jpg';
 import {PiMedalFill} from 'react-icons/pi';
-import AdminSidebar from "../../components/AdminSidebar";
-import AdminNavbar from "../../components/AdminNavbar";
-import {FaUserEdit} from "react-icons/fa";
-import { Link } from "react-router-dom";
+import ReceiptionistSidebar from "../../components/ReceiptionistSidebar";
+import ReceiptionistNavbar from "../../components/ReceiptionistNavbar";
 
-const Profile = () => {
+const Instructors = () => {
+
+
 
   return (
 
 
     <Box sx={{ flex: "1", display:"flex", mb:2}}>
       <Box>
-        <AdminSidebar />
+        <ReceiptionistSidebar />
       </Box>
       
       <Box component="main" sx={{flex:1 }}>
         <Box>
-          <AdminNavbar />
+          <ReceiptionistNavbar />
         </Box>
         <Box sx={{ paddingLeft:"5rem", flex:1 }}>
            
         <Typography variant="h4" style={{ fontWeight: 700, marginTop: "1rem", textAlign:"left" }}>Profile</Typography>
         <Box sx={{ display:"flex", width: "100%", height:"100%"}}> 
-            <Box sx={{ width: "50%", height:"100%", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
-                <img src={ProfileImg} alt="Profile" width="45%"  style={{borderRadius:"10px", marginTop:"0.5rem"}}/>
+            <Box sx={{ width: "60%", height:"100%", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
+                <img src={ProfileImg} alt="Profile" width="55%" height="400px" style={{borderRadius:"10px", marginTop:"3rem"}}/>
             </Box>
             <Box sx={{ width: "50%", height:"100%", flexDirection:"column", marginLeft:"0%", justifyContent:"center"}}>
                 
-                <Link to="/member/editprofile" style={{textDecoration:"none", color:"black"}}>
-                  <Box style={{display:"flex"}}>
-                    <Typography variant="h5" style={{ fontWeight: 700, textAlign:"left", marginLeft:"-5rem" }}><PiMedalFill style={{marginTop: "1rem"}}/>  Personal Information</Typography>
-                    <FaUserEdit size={20} style={{marginTop:"3.5%", marginLeft:"40%", marginRight:"1%"}}/>
-                    <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 700, textAlign:"right", marginLeft:"0%", marginTop:"3.2%" }}> Edit Profile</Typography>
-                  </Box>  
-                </Link>
-                <Box sx={{display: "flex", marginLeft:"4rem"}}>
-                    <Box >
-                        <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-57%" }}>Name: Tharindu Gunawardhana</Typography>
-                        <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-57%" }}>Age: 29</Typography>
-                        <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-57%" }}>NIC: 986542135V</Typography>
-                        <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-57%" }}>DOB: 21-10-1998</Typography>
-                        <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-57%" }}>Gender: Male</Typography>
-                    </Box>
+                <Typography variant="h5" style={{ fontWeight: 700, textAlign:"left", marginLeft:"-15rem" }}><PiMedalFill style={{marginTop: "5rem"}}/>  Personal Information</Typography>
+                <Box sx={{display: "flex"}}>
+
                     <Box>
-                        <Typography variant="h6" style={{fontSize:"16px", fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-21%" }}>Weight: 70 KG</Typography>
-                        <Typography variant="h6" style={{fontSize:"16px", fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-21%" }}>Height: 180cm</Typography>
-                        <Typography variant="h6" style={{fontSize:"16px", fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-21%" }}>Membership ID: M0231564</Typography>
-                        <Typography variant="h6" style={{fontSize:"16px", fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-21%" }}>Membership Type: 6 Month Plan</Typography>
-                       
+                        <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-130%" }}>Name: Anjana Silva</Typography>
+                        <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-130%" }}>Age: 25</Typography>
+                        <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-130%" }}>NIC: 986542135V</Typography>
+                        <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-130%" }}>DOB: 21-10-1998</Typography>
+                        <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"-130%" }}>Gender: Female</Typography>
                     </Box>
-                </Box>	
-                
-            </Box>
-            
-        </Box>
-        <Box sx={{display:"flex", width: "100%", height:"50%", textAlign:"left", marginTop:"2rem"}}>
-            <Box style={{width:"40%",}}>
-                <Typography variant="h5" style={{ fontWeight: 700, marginTop: "1rem", marginLeft: "", }}><FaTelegram style={{marginTop: "1rem"}}/>  Contact Information</Typography>
+
+                    <Box style={{width:"50%",}}>
+                <Typography variant="h5" style={{ fontWeight: 700, marginTop: "-3rem",  }}><FaTelegram style={{marginTop: "1rem"}}/>  Contact Information</Typography>
                 <Box style={{marginLeft:"1%"}}>
                     <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left"}}>Address: 6th Flr Paul CI Cent 24 Malwatte Road, 11</Typography>
-                    <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left"}}>Email:tharindu@123gmail.com</Typography>
+                    <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left"}}>Email:dula@123gmail.com</Typography>
                     <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left"}}>Contact Number: (+94) (76) 156 2514</Typography>
                     <Typography variant="h6" style={{ fontSize:"16px",fontWeight: 500, marginTop: "1rem", textAlign:"left"}}>Emergency Contact: (+94) (76) 156 2514</Typography>
                 
                 </Box>
             </Box>
-            <Box  style={{width:"60%", marginLeft:"6%"}}>
-                <Typography variant="h5" style={{ fontWeight: 700, marginTop: "1rem",  }}><FaFeatherAlt style={{marginTop: "1rem"}}/>  Other</Typography>
-                <Box style={{marginLeft:"1%"}}>
-                    <Typography variant="h6" style={{fontSize:"16px", fontWeight: 500, marginTop: "1rem", textAlign:"left" }}>Medical Issues: </Typography>
-                    <Typography variant="h6" style={{fontSize:"16px", fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"5%"}}>Taking medicine for Cholesterol since 3 years</Typography>
-                    <Typography variant="h6" style={{fontSize:"16px", fontWeight: 500, marginTop: "1rem", textAlign:"left", marginLeft:"5%"}}>Taking medicine for diabetic since 5 years </Typography>
-            
-                </Box>
+                </Box>	
+                
             </Box>
-           
-        </Box>        
-    
+            
+        </Box>
+
         
         </Box>
       </Box>
@@ -91,4 +68,4 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+export default Instructors;
