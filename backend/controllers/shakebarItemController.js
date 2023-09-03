403f538afb27947f71e35c443c3c060fe59169b1
anjanasilva99 <108554413+anjanasilva99@users.noreply.github.com>
2023-09-03 12:27:59 +0530

    Create shakebarItemController.js
    create controller for upload and view shakebar items

---
+++ backend/controllers/shakebarItemController.js
@@ -0,0 +1,43 @@
+import asyncHandler from 'express-async-handler';
+import {
+    getShakeBarItems,
+    addShakeBarItem
+} from "../models/shakebarItemsModal.js";
+
+
+// get all shakebar items 
+const getAllShakeBarItems = asyncHandler(async (req, res) => {
+    const shakebarItems = await getShakeBarItems();
+
+    if (shakebarItems === undefined) {
+        res.status(500);
+        throw new Error("Something went wrong!");
+    }
+
+    res.status(200).json({
+        data: shakebarItems,
+    });
+});
+
+
+// add shakebar item
+const addNewShakeBarItem = asyncHandler(async (req, res) => {
+    const { name, unitPrice, category, quantity, description} = req.body;
+    const added_by = req.user.id;
+    const result = await addShakeBarItem(name, unitPrice, category, quantity, description, added_by);
+
+    if (! result) {
+        res.status(500);
+        throw new Error("Something went wrong!");
+    }
+
+    res.status(201).json({
+        data: result,
+        message: "Shakebar item added successfully.",
+    });
+});
+
+export {
+    getAllShakeBarItems,
+    addNewShakeBarItem,
+};
