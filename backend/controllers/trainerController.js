273da8fe55f70b13b96e28f2c9c8b11b561f7c10
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-09-10 11:06:53 +0530

    Setup trainer component
    Update trainer routes, controller and model to create add trainer feature

--- backend/controllers/trainerController.js
+++ backend/controllers/trainerController.js
@@ -1,8 +1,18 @@
 import asyncHandler from 'express-async-handler';
 import {
     getViewTrainers,
-    
+    addTrainer,
 } from "../models/trainerModel.js";
+import {
+    findUserById,
+    findUserByNIC,
+    findUserByEmail,
+    findUserByContactNo,
+} from "../models/userModel.js";
+import generateUserId from "../utils/generateUserId.js";
+import generatePassword from "../utils/generatePassword.js";
+import sendEmail from "../utils/sendEmail.js";
+import { getSubject, getBody } from "../utils/EmpRegMailTemplate.js";
 
 
 //view all trainers
@@ -19,7 +29,53 @@ const getViewAllTrainers = asyncHandler(async (req, res) => {
     });
 });
 
+
+//add trainer
+const addNewTrainer = asyncHandler(async (req, res) => {
+    // const { nic, first_name, last_name, email, contact_no} = req.body;
+    //
+    // if (await findUserByNIC(nic)) {
+    //     res.status(409);    // status code for conflict
+    //     throw new Error("NIC already exists.");
+    // }
+    //
+    // if (await findUserByEmail(email)) {
+    //     res.status(409);
+    //     throw new Error("Email already exists.");
+    // }
+    //
+    // if (await findUserByContactNo(contact_no)) {
+    //     res.status(409);
+    //     throw new Error("Contact no already exists.");
+    // }
+    //
+    // const addedBy = req.body.userID;
+    //
+    // let id;
+    // do {
+    //     id = generateUserId('Receptionist');
+    // } while (await findUserById(id));
+    //
+    // const password = generatePassword();
+    //
+    // const subject = getSubject();
+    // const body = getBody(first_name, email, password);
+    //
+    // if (await addReceptionist(id, nic, first_name, last_name, email, password, contact_no, addedBy) &&
+    //     await sendEmail(subject, body, email)) {
+    //     res.status(201).json({
+    //         message: "Receptionist added successfully.",
+    //     });
+    // } else {
+    //     res.status(500);
+    //     throw new Error("Something went wrong!");
+    // }
+    console.log("addNewTrainer controller");
+    await addTrainer();
+});
+
+
 export {
     getViewAllTrainers,
-    
+    addNewTrainer,
 };
--- backend/models/trainerModel.js
+++ backend/models/trainerModel.js
@@ -10,6 +10,20 @@ const getViewTrainers = asyncHandler(async () => {
     return result.rows;
 });
 
+
+// add trainer
+const addTrainer = asyncHandler(async () => {
+    // const hashedPassword = await hashPassword(password);
+    //
+    // const sql = 'INSERT INTO receptionist (id, nic, first_name, last_name, email, password, contact_no, added_by) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id';
+    // const result = await query(sql, [id, nic, firstName, lastName, email, hashedPassword, contactNo, addedBy]);
+    //
+    // return result.rows[0].id;
+    console.log("addTrainer model");
+});
+
+
 export{
     getViewTrainers,
+    addTrainer,
 };
