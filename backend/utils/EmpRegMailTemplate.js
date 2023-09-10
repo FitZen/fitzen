5ec0b9a812fadd78817e3e593a554eab26fae8d3
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-09-10 11:46:44 +0530

    Update EmpRegMailTemplate.js

--- backend/utils/EmpRegMailTemplate.js
+++ backend/utils/EmpRegMailTemplate.js
@@ -34,11 +34,6 @@ const getBody = (firstName, email, password) => {
                     <li>Always log out when you've finished your session.</li>
                 </ul>
         
-                <p>
-                    If you did not request this account or believe this email was sent in error, please get in touch with us
-                    through our contact page or platform.
-                </p>
-        
                 <p>Thank you for being a part of FitZen.</p>
         
                 <p>Best regards,<br>FitZen Support Team</p>
