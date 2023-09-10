0b6591c3e5e483bdef36a039f6d49daab3f4cf2a
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-09-10 14:56:58 +0530

    Setup route, controller and model to add physiotherapist

--- backend/controllers/physiotherapistController.js
+++ backend/controllers/physiotherapistController.js
@@ -1,8 +1,18 @@
 import asyncHandler from 'express-async-handler';
 import {
     getViewPhysiotherapists,
-    
+    addPhysiotherapist,
 } from "../models/physiotherapistModel.js";
+import {
+    findUserById,
+    findUserByNIC,
+    findUserByEmail,
+    findUserByContactNo,
+} from "../models/userModel.js";
+import generateUserId from "../utils/generateUserId.js";
+import generatePassword from "../utils/generatePassword.js";
+import sendEmail from "../utils/sendEmail.js";
+import { getSubject, getBody } from "../utils/EmpRegMailTemplate.js";
 
 
 //view all trainers
@@ -19,7 +29,53 @@ const getViewAllPhysiotherapists = asyncHandler(async (req, res) => {
     });
 });
 
+
+//add physiotherapist
+const addNewPhysiotherapist = asyncHandler(async (req, res) => {
+    // const { nic, first_name, last_name, email, contact_no, address, dob, gender, qualification, mode} = req.body;
+    //
+    // if (await findUserByNIC(nic)) {
+    //     res.status(409);    // status code for conflict
+    //     throw new Error("NIC already exists.");
+    // }
+    //
+    // if (await findUserByEmail(email)) {
+    //     res.status(409);
+    //     throw new Error("Email already exists.");
+    // }
+    //
+    // if (await findUserByContactNo(contact_no)) {
+    //     res.status(409);
+    //     throw new Error("Contact no already exists.");
+    // }
+    //
+    // const addedBy = req.user.id;
+    //
+    // let id;
+    // do {
+    //     id = generateUserId('Trainer');
+    // } while (await findUserById(id));
+    //
+    // const password = generatePassword();
+    //
+    // const subject = getSubject();
+    // const body = getBody(first_name, email, password);
+    //
+    // if (await addTrainer(id, nic, first_name, last_name, email, password, contact_no, address, dob, gender, qualification, mode, addedBy) &&
+    //     await sendEmail(subject, body, email)) {
+    //     res.status(201).json({
+    //         message: "Trainer added successfully.",
+    //     });
+    // } else {
+    //     res.status(500);
+    //     throw new Error("Something went wrong!");
+    // }
+    console.log("addNewPhysiotherapist controller");
+    await addPhysiotherapist();
+});
+
+
 export {
     getViewAllPhysiotherapists,
-    
+    addNewPhysiotherapist,
 };
--- backend/models/physiotherapistModel.js
+++ backend/models/physiotherapistModel.js
@@ -1,5 +1,6 @@
 import {query} from '../config/db.js';
 import asyncHandler from 'express-async-handler';
+import hashPassword from "../utils/hashPassword.js";
 
 
 //view all trainers
@@ -10,6 +11,20 @@ const getViewPhysiotherapists = asyncHandler(async () => {
     return result.rows;
 });
 
+
+// add physiotherapist
+const addPhysiotherapist = asyncHandler(async () => {
+    // const hashedPassword = await hashPassword(password);
+    //
+    // const sql = 'INSERT INTO trainer (id, nic, first_name, last_name, email, password, contact_no, address, dob, gender, qualification, mode, added_by) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING id;';
+    // const result = await query(sql, [id, nic, first_name, last_name, email, hashedPassword, contact_no, address, dob, gender, qualification, mode, addedBy]);
+    //
+    // return result.rows[0].id;
+    console.log("addPhysiotherapist model");
+});
+
+
 export{
-    getViewPhysiotherapists
+    getViewPhysiotherapists,
+    addPhysiotherapist,
 };
