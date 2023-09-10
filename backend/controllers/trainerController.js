657c87e44a37e91802df91f44f688bf6bd115cb9
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-09-10 12:12:26 +0530

    Complete add trainer operation

--- backend/controllers/trainerController.js
+++ backend/controllers/trainerController.js
@@ -32,46 +32,44 @@ const getViewAllTrainers = asyncHandler(async (req, res) => {
 
 //add trainer
 const addNewTrainer = asyncHandler(async (req, res) => {
-    // const { nic, first_name, last_name, email, contact_no} = req.body;
-    //
-    // if (await findUserByNIC(nic)) {
-    //     res.status(409);    // status code for conflict
-    //     throw new Error("NIC already exists.");
-    // }
-    //
-    // if (await findUserByEmail(email)) {
-    //     res.status(409);
-    //     throw new Error("Email already exists.");
-    // }
-    //
-    // if (await findUserByContactNo(contact_no)) {
-    //     res.status(409);
-    //     throw new Error("Contact no already exists.");
-    // }
-    //
-    // const addedBy = req.body.userID;
-    //
-    // let id;
-    // do {
-    //     id = generateUserId('Receptionist');
-    // } while (await findUserById(id));
-    //
-    // const password = generatePassword();
-    //
-    // const subject = getSubject();
-    // const body = getBody(first_name, email, password);
-    //
-    // if (await addReceptionist(id, nic, first_name, last_name, email, password, contact_no, addedBy) &&
-    //     await sendEmail(subject, body, email)) {
-    //     res.status(201).json({
-    //         message: "Receptionist added successfully.",
-    //     });
-    // } else {
-    //     res.status(500);
-    //     throw new Error("Something went wrong!");
-    // }
-    console.log("addNewTrainer controller");
-    await addTrainer();
+    const { nic, first_name, last_name, email, contact_no, address, dob, gender, qualification, mode} = req.body;
+
+    if (await findUserByNIC(nic)) {
+        res.status(409);    // status code for conflict
+        throw new Error("NIC already exists.");
+    }
+
+    if (await findUserByEmail(email)) {
+        res.status(409);
+        throw new Error("Email already exists.");
+    }
+
+    if (await findUserByContactNo(contact_no)) {
+        res.status(409);
+        throw new Error("Contact no already exists.");
+    }
+
+    const addedBy = req.user.id;
+
+    let id;
+    do {
+        id = generateUserId('Trainer');
+    } while (await findUserById(id));
+
+    const password = generatePassword();
+
+    const subject = getSubject();
+    const body = getBody(first_name, email, password);
+
+    if (await addTrainer(id, nic, first_name, last_name, email, password, contact_no, address, dob, gender, qualification, mode, addedBy) &&
+        await sendEmail(subject, body, email)) {
+        res.status(201).json({
+            message: "Trainer added successfully.",
+        });
+    } else {
+        res.status(500);
+        throw new Error("Something went wrong!");
+    }
 });
 
 
--- backend/models/trainerModel.js
+++ backend/models/trainerModel.js
@@ -1,5 +1,6 @@
 import {query} from '../config/db.js';
 import asyncHandler from 'express-async-handler';
+import hashPassword from "../utils/hashPassword.js";
 
 
 //view all trainers
@@ -12,14 +13,13 @@ const getViewTrainers = asyncHandler(async () => {
 
 
 // add trainer
-const addTrainer = asyncHandler(async () => {
-    // const hashedPassword = await hashPassword(password);
-    //
-    // const sql = 'INSERT INTO receptionist (id, nic, first_name, last_name, email, password, contact_no, added_by) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id';
-    // const result = await query(sql, [id, nic, firstName, lastName, email, hashedPassword, contactNo, addedBy]);
-    //
-    // return result.rows[0].id;
-    console.log("addTrainer model");
+const addTrainer = asyncHandler(async (id, nic, first_name, last_name, email, password, contact_no, address, dob, gender, qualification, mode, addedBy) => {
+    const hashedPassword = await hashPassword(password);
+
+    const sql = 'INSERT INTO trainer (id, nic, first_name, last_name, email, password, contact_no, address, dob, gender, qualification, mode, added_by) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING id;';
+    const result = await query(sql, [id, nic, first_name, last_name, email, hashedPassword, contact_no, address, dob, gender, qualification, mode, addedBy]);
+
+    return result.rows[0].id;
 });
 
 
