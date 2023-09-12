33bd8d9ca9448829368d6ee91ae9683ebe677108
Tharindu99 <91658088+tharindugunawardhana99@users.noreply.github.com>
2023-09-12 12:12:21 +0530

    Update InstructorList.jsx
    Set o add new trainer and physiotherapist

--- frontend/src/pages/admin/InstructorList.jsx
+++ frontend/src/pages/admin/InstructorList.jsx
@@ -10,7 +10,7 @@ import axios from 'axios';
 import {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-
+import Swal from "sweetalert2";
 
 const InstructorList = () => {
 
@@ -21,10 +21,30 @@ const InstructorList = () => {
   const color4 = "#DC1E2A" //red 
 
   const [fixedNavbar, setFixedNavbar] = useState(false);
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [openTrainer, setOpenTrainer] = React.useState(false);
+  const handleTrainerOpen = () => setOpenTrainer(true);
+  const handleTrainerClose = () => setOpenTrainer(false);
+  const [openPhysiotherapist, setOpenPhysiotherapist] = React.useState(false);
+  const handlePhysiotherapistOpen = () => setOpenPhysiotherapist(true);
+  const handlePhysiotherapistClose = () => setOpenPhysiotherapist(false);
   const [InstructorData, setInstructorData] = useState([]);
+
+  const [newTrainer, setNewTrainer] = useState({
+    first_name: "",
+    last_name: "",
+    nic: "",
+    email: "",
+    contact_no: "",
+  });
+  const [newPhysiotherapist, setNewPhysiotherapist] = useState({
+    first_name: "",
+    last_name: "",
+    nic: "",
+    email: "",
+    contact_no: "",
+  });
+
+  const [submitted, setSubmitted] = useState(false);
  
   const navigate = useNavigate();
   const [item, setItem] = React.useState('');
@@ -97,6 +117,128 @@ const InstructorList = () => {
     }
   };
 
+   //get form inputs
+   const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewTrainer((prevTrainer) => ({
+      ...prevTrainer,
+      [name]: value,
+    }));
+  };
+
+  const handleShakeInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewPhysiotherapist((prevPhysiotherapist) => ({
+      ...prevPhysiotherapist,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmitTrainer = async () => {
+
+    if (!newTrainer.first_name || !newTrainer.last_name || !newTrainer.nic || !newTrainer.email || !newTrainer.contact_no) {
+      // Display error messages or styles for empty fields
+      setSubmitted(true);
+      return;
+    }  
+
+    try {
+      const payload = {
+        first_name: newTrainer.first_name,
+        last_name: newTrainer.last_name,
+        nic: newTrainer.nic,
+        email: newTrainer.email,
+        contact_no: newTrainer.contact_no,
+        userID: JSON.parse(localStorage.getItem("userID")),	
+      };
+
+      console.log(payload);
+
+      const res = await axios.post(
+        "http://localhost:8000/api/trainers/addtrainer",
+        payload
+      );
+  
+      if (res.status === 201) {
+        handleTrainerClose(); // Close the modal
+        viewTrainerList(); // Refresh the announcement list
+        setNewTrainer({ first_name:"", last_name:"", nic:"", email:"",contact_no:"" }); // Clear the form
+        setSubmitted(false);
+      }
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Receptionist Added Successfully',
+        showConfirmButton: false,
+        timer: 1500
+      })
+
+    } catch (error) {
+      console.error("Adding Reception failed:", error);
+      // Handle error scenarios here
+    }
+  };
+
+  const handleSubmitPhysiotherapist = async () => {
+
+    if (!newPhysiotherapist.first_name || !newPhysiotherapist.last_name || !newPhysiotherapist.nic || !newPhysiotherapist.email || !newPhysiotherapist.contact_no) {
+      // Display error messages or styles for empty fields
+      setSubmitted(true);
+      return;
+    }  
+
+    try {
+      const payload = {
+        first_name: newPhysiotherapist.first_name,
+        last_name: newPhysiotherapist.last_name,
+        nic: newPhysiotherapist.nic,
+        email: newPhysiotherapist.email,
+        contact_no: newPhysiotherapist.contact_no,
+        userID: JSON.parse(localStorage.getItem("userID")),	
+      };
+
+      console.log(payload);
+
+      const res = await axios.post(
+        "http://localhost:8000/api/physiotherapists/addphysiotherapist",
+        payload
+      );
+  
+      if (res.status === 201) {
+        handlePhysiotherapistClose(); // Close the modal
+        viewPhysiotherapistList(); // Refresh the announcement list
+        setNewPhysiotherapist({ first_name:"", last_name:"", nic:"", email:"",contact_no:"" }); // Clear the form
+        setSubmitted(false);
+      }
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Virt Added Successfully',
+        showConfirmButton: false,
+        timer: 1500
+      })
+
+    } catch (error) {
+      console.error("Adding Reception failed:", error);
+      // Handle error scenarios here
+    }
+  };
+
+  
+
+  const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: "35%",
+    bgcolor: 'background.paper',
+    borderRadius: '10px',
+    boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px',
+    p: 4,
+  };
+
+
   return (
 
 
@@ -116,7 +258,7 @@ const InstructorList = () => {
            
           <Box sx={{display:"flex", marginTop:"5rem", justifyContent:"space-between"}}>
             <Typography variant="h4" style={{ fontWeight: 700, textAlign:"left" }}>{instructorType}s</Typography>
-
+            <Button variant="contained" onClick={instructorType === "Trainer" ? handleTrainerOpen : handlePhysiotherapistOpen} sx={{backgroundColor: color2, marginRight:"7%", marginBottom:"3%",'&:hover': {backgroundColor:color3, transition: "ease 0.5s"}}}><FaPlus />&nbsp; Add New</Button>
           </Box>
 
           <Box sx={{width:"93%", height:"80vh",marginTop:"2%", overflowY:"auto", flexWrap:"wrep", boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px' }}>
@@ -164,6 +306,91 @@ const InstructorList = () => {
           </Box>    
         </Box>
       </Box>
+
+      <Modal
+                    open={openTrainer}
+                    onClose={handleTrainerClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={modalStyle}>
+                        <FaRegTimesCircle onClick={handleTrainerClose} style={{float:"right", cursor:"pointer", fontSize:"1.5rem", color:"#D8D9DA" ,}}  
+                            onMouseEnter={(e) => {
+                                e.target.style.color = "#D71313";
+                                e.target.style.transform = "scale(1)";
+                            }}
+                            onMouseLeave={(e) => {
+                                e.target.style.color = "#D8D9DA";
+                                e.target.style.transform = "scale(1)";
+                            }}
+                        />
+                        <Box sx={{display:"flex", textAlign:"center", justifyContent:"center"}}>
+                            
+                        </Box>
+                        
+                        <Box sx={{textAlign:"center", padding:"1%"}}>
+                            <InputLabel variant="body2" style={{ fontWeight: 500, marginTop: "5%", textAlign:"left", marginLeft:"4%",color:"#000000" }}>First Name:</InputLabel>
+                            <TextField variant="outlined" name="first_name" value={newTrainer.first_name} onChange={handleInputChange} error={submitted && !newTrainer.first_name} helperText={submitted && !newTrainer.first_name ? "Title is required" : ""} inputProps={{style: {height: 1, width:400,border:"1px solid D8D9DA", borderRadius:"5px", outline:"none"}}}/>
+                            <InputLabel variant="body2" style={{ fontWeight: 500, marginTop: "3%", textAlign:"left", marginLeft:"4%", color:"#000000" }}>Last Name:</InputLabel>
+                            <TextField variant="outlined" name="last_name" value={newTrainer.last_name} onChange={handleInputChange} error={submitted && !newTrainer.last_name} helperText={submitted && !newTrainer.last_name ? "Title is required" : ""} inputProps={{style: {height: 1, width:400,border:"1px solid D8D9DA", borderRadius:"5px", outline:"none"}}}/>
+                            <InputLabel variant="body2" style={{ fontWeight: 500, marginTop: "3%", textAlign:"left", marginLeft:"4%", color:"#000000" }}>NIC:</InputLabel>
+                            <TextField variant="outlined" name="nic" value={newTrainer.nic} onChange={handleInputChange} error={submitted && !newTrainer.nic} helperText={submitted && !newTrainer.nic ? "Title is required" : ""} inputProps={{style: {height: 1, width:400,border:"1px solid D8D9DA", borderRadius:"5px", outline:"none"}}}/>
+                            <InputLabel variant="body2" style={{ fontWeight: 500, marginTop: "3%", textAlign:"left", marginLeft:"4%", color:"#000000" }}>Email:</InputLabel>
+                            <TextField variant="outlined" name="email" value={newTrainer.email} onChange={handleInputChange} error={submitted && !newTrainer.email} helperText={submitted && !newTrainer.email ? "Title is required" : ""} inputProps={{style: {height: 1, width:400,border:"1px solid D8D9DA", borderRadius:"5px", outline:"none"}}}/>
+                            <InputLabel variant="body2" style={{ fontWeight: 500, marginTop: "3%", textAlign:"left", marginLeft:"4%", color:"#000000" }}>Contact No:</InputLabel>
+                            <TextField variant="outlined" name="contact_no" value={newTrainer.contact_no} onChange={handleInputChange} error={submitted && !newTrainer.contact_no} helperText={submitted && !newTrainer.contact_no ? "Title is required" : ""} inputProps={{style: {height: 1, width:400,border:"1px solid D8D9DA", borderRadius:"5px", outline:"none"}}}/>
+                            
+                            <Box sx={{display:"flex", marginTop:"3%", justifyContent:"center"}}>
+                              <Button onClick={handleSubmitTrainer} variant="contained" style={{backgroundColor:color2, color:"white", marginTop:"7%", marginBottom:"1%", marginRight:"1%"}}>Add Trainer</Button>
+                            </Box>
+                            
+                           
+                        </Box>
+                        
+                    </Box>
+                </Modal>
+                <Modal
+                    open={openPhysiotherapist}
+                    onClose={handlePhysiotherapistClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={modalStyle}>
+                        <FaRegTimesCircle onClick={handlePhysiotherapistClose} style={{float:"right", cursor:"pointer", fontSize:"1.5rem", color:"#D8D9DA" ,}}  
+                            onMouseEnter={(e) => {
+                                e.target.style.color = "#D71313";
+                                e.target.style.transform = "scale(1)";
+                            }}
+                            onMouseLeave={(e) => {
+                                e.target.style.color = "#D8D9DA";
+                                e.target.style.transform = "scale(1)";
+                            }}
+                        />
+                        <Box sx={{display:"flex", textAlign:"center", justifyContent:"center"}}>
+                            
+                        </Box>
+                        
+                        <Box sx={{textAlign:"center", padding:"1%"}}>
+                            <InputLabel variant="body2" style={{ fontWeight: 500, marginTop: "5%", textAlign:"left", marginLeft:"4%",color:"#000000" }}>First Name:</InputLabel>
+                            <TextField variant="outlined" name="first_name" value={newPhysiotherapist.first_name} onChange={handleShakeInputChange} error={submitted && !newPhysiotherapist.first_name} helperText={submitted && !newPhysiotherapist.first_name ? "Title is required" : ""} inputProps={{style: {height: 1, width:400,border:"1px solid D8D9DA", borderRadius:"5px", outline:"none"}}}/>
+                            <InputLabel variant="body2" style={{ fontWeight: 500, marginTop: "3%", textAlign:"left", marginLeft:"4%", color:"#000000" }}>Last Name:</InputLabel>
+                            <TextField variant="outlined" name="last_name" value={newPhysiotherapist.last_name} onChange={handleShakeInputChange} error={submitted && !newPhysiotherapist.last_name} helperText={submitted && !newPhysiotherapist.last_name ? "Title is required" : ""} inputProps={{style: {height: 1, width:400,border:"1px solid D8D9DA", borderRadius:"5px", outline:"none"}}}/>
+                            <InputLabel variant="body2" style={{ fontWeight: 500, marginTop: "3%", textAlign:"left", marginLeft:"4%", color:"#000000" }}>NIC:</InputLabel>
+                            <TextField variant="outlined" name="nic" value={newPhysiotherapist.nic} onChange={handleShakeInputChange} error={submitted && !newPhysiotherapist.nic} helperText={submitted && !newPhysiotherapist.nic ? "Title is required" : ""} inputProps={{style: {height: 1, width:400,border:"1px solid D8D9DA", borderRadius:"5px", outline:"none"}}}/>
+                            <InputLabel variant="body2" style={{ fontWeight: 500, marginTop: "3%", textAlign:"left", marginLeft:"4%", color:"#000000" }}>Email:</InputLabel>
+                            <TextField variant="outlined" name="email" value={newPhysiotherapist.email} onChange={handleShakeInputChange} error={submitted && !newPhysiotherapist.email} helperText={submitted && !newPhysiotherapist.email ? "Title is required" : ""} inputProps={{style: {height: 1, width:400,border:"1px solid D8D9DA", borderRadius:"5px", outline:"none"}}}/>
+                            <InputLabel variant="body2" style={{ fontWeight: 500, marginTop: "3%", textAlign:"left", marginLeft:"4%", color:"#000000" }}>Contact No:</InputLabel>
+                            <TextField variant="outlined" name="contact_no" value={newPhysiotherapist.contact_no} onChange={handleShakeInputChange} error={submitted && !newPhysiotherapist.contact_no} helperText={submitted && !newPhysiotherapist.contact_no ? "Title is required" : ""} inputProps={{style: {height: 1, width:400,border:"1px solid D8D9DA", borderRadius:"5px", outline:"none"}}}/>
+                            
+                            <Box sx={{display:"flex", marginTop:"3%", justifyContent:"center"}}>
+                              <Button onClick={handleSubmitPhysiotherapist}  variant="contained" style={{backgroundColor:color2, color:"white", marginTop:"7%", marginBottom:"1%", marginRight:"1%"}}>Add Physiotherapist</Button>
+                            </Box>
+                            
+                           
+                        </Box>
+                        
+                    </Box>
+                </Modal>
      
     </Box>
 
