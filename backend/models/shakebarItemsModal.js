df6637414e1d9e387116084ebf346e30e94fa79d
Dula0133 <92773152+Dula0133@users.noreply.github.com>
2023-09-13 23:50:21 +0530

    changed get shakebar items

--- backend/models/shakebarItemsModal.js
+++ backend/models/shakebarItemsModal.js
@@ -5,7 +5,7 @@ import asyncHandler from 'express-async-handler';
 
 // get all shakebar items
 const getShakeBarItems = asyncHandler(async () => {
-    const sql = 'SELECT sbi.id, sbi.name, itm.availble_count, sbi.unit_price, sbi.category, sbi.description, sbi.image FROM shakeBarItems sbi INNER JOIN itemDetails itm ON sbi.id = itm.id ORDER BY sbi.id DESC;'
+    const sql = 'SELECT * FROM shakeBarItems;'
     const result = await query(sql);
 
     return result.rows;
@@ -13,16 +13,12 @@ const getShakeBarItems = asyncHandler(async () => {
 
 
 // add shakebar item
-const addShakeBarItem = asyncHandler(async (name, unitPrice, category, quantity, description, added_by) => {
-    const sql = 'INSERT INTO shakeBarItems (name, unit_price, category, description, added_by) VALUES ($1, $2, $3, $4, $5) RETURNING id';
-    const result = await query(sql, [name, unitPrice, category, description, added_by]);
+const addShakeBarItem = asyncHandler(async (name, unitPrice, category, quantity, description) => {
+    const sql = 'INSERT INTO shakeBarItems (name, unit_price, quantity, category, description) VALUES ($1, $2, $3, $4,$5) RETURNING id';
+    const result = await query(sql, [name, unitPrice, quantity, category, description]);
 
-    const itemId = result.rows[0].id;
-    
-    const sql2 = 'INSERT INTO itemDetails (id, availble_count, updated_by) VALUES ($1, $2 ,$3) RETURNING id';
-    const result2 = await query(sql2, [itemId, quantity, added_by]);
 
-    return result.rows[0].id, result2.rows[0].id;
+    return result.rows[0].id;
 });
 
 
