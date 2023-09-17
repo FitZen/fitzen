9c558d8e897b697720fc4df370b8706eb1c840dc
Tharindu99 <91658088+tharindugunawardhana99@users.noreply.github.com>
2023-09-17 17:04:58 +0530

    Set to view announcements & notifications to view trainer

--- frontend/src/pages/trainer/Notification.jsx
+++ frontend/src/pages/trainer/Notification.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
+import { Typography, Tab, Tabs } from "@mui/material";
 import Sidebar from "../../components/TrainerSidebar";
 import Navbar from "../../components/TrainerNavbar";
 import CheckIcon from '@mui/icons-material/Check';
@@ -12,6 +12,7 @@ import axios from 'axios';
 const TrainerNotification = () => {
     const [selectedMap, setSelectedMap] = React.useState({});
     const [fixedNavbar, setFixedNavbar] = useState(false);
+    const [announcementData, setAnnouncementData] = useState([]); 
     const [notificationData, setNotificationData] = useState([]);
   
     const handleToggle = (notificationId) => {
@@ -22,14 +23,21 @@ const TrainerNotification = () => {
     };
   
     const navigate = useNavigate();
+
+    const [selectedTab, setSelectedTab] = useState(0);
+  
+    const handleTabChange = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
   
     useEffect(() => {
-  
-      viewNotifications();
-  
-      if(!localStorage.getItem('userID')){
+
+      if((localStorage.getItem('userType') !== '"Trainer"')){
         navigate('/login');
       }
+  
+      viewAnnouncements();
+  
       // Function to handle scroll event
       const handleScroll = () => {
         if (window.scrollY > 0) {
@@ -48,10 +56,24 @@ const TrainerNotification = () => {
       };
     }, []);
   
-    const viewNotifications = async () => {
+    const viewAnnouncements = async () => {
       try {
        
         const res = await axios.get("http://localhost:8000/api/announcements/getannouncements");
+        console.log(res.data.data);
+        setAnnouncementData(res.data.data);
+  
+        // Perform any additional actions after successful logout, such as clearing local storage, redirecting, etc.
+      } catch (error) {
+        console.error("Retrieving failed:", error);
+        // Handle error scenarios here
+      }
+    };
+
+    const viewNotifications = async () => {
+      try {
+       
+        const res = await axios.get("http://localhost:8000/api/");
         console.log(res.data.data);
         setNotificationData(res.data.data);
   
@@ -76,12 +98,39 @@ const TrainerNotification = () => {
             <Navbar />
           </div>
           <Box sx={{ paddingLeft:"5rem", flex:1 }}>
-            <Typography variant="h4" style={{ fontWeight: 700, marginTop: "5rem", textAlign:"left" }}>Notification</Typography>
+            <Typography variant="h4" style={{ fontWeight: 700, marginTop: "5rem", textAlign:"left" }}>Announcements & Notifications</Typography>
+            <Tabs value={selectedTab} onChange={handleTabChange} aria-label="Report Tabs" style={{ backgroundColor: '#ffffff', width:"24%",marginTop:"2%", marginBottom:"2%", borderRadius:"5px", boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}>
+                <Tab label="Announcement" onClick={viewAnnouncements}/>
+                <Tab label="Notification" onClick={viewNotifications}/>
+              </Tabs>
             <Box sx={{  width: "90%", height:"80vh", padding:"1rem", overflowY:"auto", flexWrap:"wrep", borderRadius:"10px", boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}> 
                 
-              {notificationData.map((notification) => (
+              {selectedTab === 0 && announcementData.map((announcement) => ( 
                 <Box sx={{display:"flex", padding:"1rem", borderRadius:"10px",marginBottom:"1rem", boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px',border:"2px solid white", '&:hover': {borderColor: '#96CDEF',  transition: "ease 0.5s"}}}>
-  
+    
+                  <Box sx={{display:"flex", flexDirection:"column",  width:"2%", borderRadius:"5px", backgroundColor:"#346E93"}}></Box>
+                  <Box sx={{marginLeft:"5%", marginRight:"5%", width:"85%", borderRadius:"10px", padding:"1rem", border:"1px solid #96CDEF"}}>
+                    <Typography variant="h6" style={{ fontWeight: 700, textAlign:"left" }}>{announcement.title}</Typography>
+                    <Typography variant="body1" style={{ fontWeight: 500, marginTop: "1rem", textAlign:"left" }}>{announcement.content}</Typography>
+                    <Typography variant="body2" style={{ fontWeight: 500, marginTop: "1rem", textAlign:"left", color: "grey", fontSize:"11px" }}>Date: {new Date(announcement.posted_on).toLocaleDateString()}</Typography>
+                  </Box>
+                  <ToggleButton
+                    value="check"
+                    selected={selectedMap[announcement.id]}
+                    onChange={() => {
+                      handleToggle(announcement.id);
+                    }}
+                    sx={{height:"50%", marginTop:"2%", color: selectedMap[announcement.id] ? "#ffffff" : "#346E93",backgroundColor: selectedMap[announcement.id] ? "#346E93" : "#ffffff"  , border:"1px solid #346E93", borderRadius:"5px", '&:hover': {backgroundColor:"#346E93",color:"#ffffff",  transition: "ease 0.5s"}}}
+                  >
+                    <CheckIcon />
+                  </ToggleButton>
+
+              </Box>
+              ) )} 
+
+              {selectedTab === 1 && notificationData.map((notification) => ( 
+                <Box sx={{display:"flex", padding:"1rem", borderRadius:"10px",marginBottom:"1rem", boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px',border:"2px solid white", '&:hover': {borderColor: '#96CDEF',  transition: "ease 0.5s"}}}>
+    
                     <Box sx={{display:"flex", flexDirection:"column",  width:"2%", borderRadius:"5px", backgroundColor:"#346E93"}}></Box>
                     <Box sx={{marginLeft:"5%", marginRight:"5%", width:"85%", borderRadius:"10px", padding:"1rem", border:"1px solid #96CDEF"}}>
                       <Typography variant="h6" style={{ fontWeight: 700, textAlign:"left" }}>{notification.title}</Typography>
@@ -98,9 +147,9 @@ const TrainerNotification = () => {
                     >
                       <CheckIcon />
                     </ToggleButton>
-  
+
                 </Box>
-              ))}
+              ) )}                 
             
                 
             </Box>
