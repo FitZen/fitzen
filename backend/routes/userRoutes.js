9483eac75a19dd2f05bf506890c1e79f44e33ace
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-10-25 12:58:10 +0530

    Setup the environment for API testing

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,14 +13,14 @@ import { protect, permit } from "../middleware/authMiddleware.js";
 // '/api/users' is connected to this file
 // therefore no need to add '/api/users' in the routes
 router.post("/login", loginUser);
-// router.post("/logout", protect, logoutUser);
-// router.get("/details", protect, getUserAllDetails);
-router.post("/logout", logoutUser);
-router.get("/details", getUserAllDetails);
+router.post("/logout", protect, logoutUser);
+router.get("/details", protect, getUserAllDetails);
+// router.post("/logout", logoutUser);
+// router.get("/details", getUserAllDetails);
 
-// router.get("/total/count", protect, permit('Admin'), getTotalUserCount);
-router.get("/total/count", getTotalUserCount);
-// router.get("/active/count", protect, permit('Admin'), getActiveUserCount);
-router.get("/active/count", getActiveUserCount);
+router.get("/total/count", protect, permit('Admin'), getTotalUserCount);
+// router.get("/total/count", getTotalUserCount);
+router.get("/active/count", protect, permit('Admin'), getActiveUserCount);
+// router.get("/active/count", getActiveUserCount);
 
 export default router;
