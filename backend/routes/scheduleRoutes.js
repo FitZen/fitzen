43f1fbf819fec47e2e97cb6bb68ff7d8e20f0276
Tharindu99 <91658088+tharindugunawardhana99@users.noreply.github.com>
2023-10-28 01:59:34 +0530

    Update scheduleRoutes.js

--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -3,12 +3,16 @@ const router = express.Router();
 import {
     getAllTaskDates,
     getAllTasksDayBased,
+    getAllCurrentDayTasks,
+    getNextTask,
     addMemberSchedule
 } from "../controllers/scheduleController.js";
 import { protect, permit } from "../middleware/authMiddleware.js";
 
 router.get('/gettasksdates', getAllTaskDates);
 router.get('/gettasksdaybased', getAllTasksDayBased);
+router.get('/getcurrentdaytasks', getAllCurrentDayTasks);
+router.get('/getnexttask', getNextTask);
 router.post('/addmemberschedule', addMemberSchedule);
 
 export default router;
