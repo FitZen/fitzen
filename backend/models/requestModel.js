3418ff44a1c1950f418a3e77f9d9d21c4491ff56
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-10-29 12:09:14 +0530

    Complete view, add & update request

--- backend/models/requestModel.js
+++ backend/models/requestModel.js
@@ -15,6 +15,49 @@ const allRequests = asyncHandler(async (trainerId) => {
 });
 
 
+// is having pending request
+const isHavingPendingRequest = asyncHandler(async (memberId) => {
+    const sql = 'SELECT * FROM request WHERE member_id = $1 AND status = \'Pending\';';
+    const result = await query(sql, [memberId]);
+
+    if (result.rowCount > 0) {
+        return true
+    } else {
+        return false;
+    }
+});
+
+
+// add a request
+const addRequest = asyncHandler(async (memberId, trainerId, packageType) => {
+    const sql = 'INSERT INTO request (member_id, trainer_id, package)\n' +
+                        'VALUES ($1, $2, $3) RETURNING id;';
+    const result = await query(sql, [memberId, trainerId, packageType]);
+
+    if (result.rowCount > 0) {
+        return result.rows[0].id;
+    } else {
+        return null;
+    }
+});
+
+
+// update request status
+const updateRequestStatus = asyncHandler(async (requestId, status) => {
+    const sql = 'UPDATE request SET status = $1 WHERE id = $2 RETURNING id;';
+    const result = await query(sql, [status, requestId]);
+
+    if (result.rowCount > 0) {
+        return result.rows[0].id;
+    } else {
+        return null;
+    }
+});
+
+
 export {
     allRequests,
+    isHavingPendingRequest,
+    addRequest,
+    updateRequestStatus,
 }
