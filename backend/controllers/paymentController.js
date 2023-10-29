f4e29b0283d2abd4411ccd5e8842cf447176ffa4
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-10-29 18:19:08 +0530

    Complete intergration testing

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -3,18 +3,60 @@ import {
     testPaymentModel
 } from "../models/paymentMode.js";
 
+import { Stripe } from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SK);
 
-// test payment controller
+
+// test payment controller (create checkout session)
 const testPaymentController = asyncHandler(async (req, res) => {
-    // const instructorId = req.query.userID;
-    // const ratings = await instructorRatings(instructorId);
-    //
-    // res.status(200).json({
-    //     data: ratings,
+    // const items = req.body.cartItems.map(item => {
+    //     return {
+    //         price_data: {
+    //             currency: 'lkr',
+    //             product_data: {
+    //                 name: item.name,
+    //                 image: [item.image],
+    //             },
+    //             unit_amount: item.price * 100,      // by default, amount is in cents
+    //         },
+    //         quantity: item.qty,
+    //     };
     // });
 
-    await testPaymentModel();
-    console.log('test payment controller');
+
+    const items = [
+        {
+            price_data: {
+                currency: 'lkr',
+                product_data: {
+                    name: 'T-shirt - Black',
+                },
+                unit_amount: 10000,      // by default, amount is in cents
+            },
+            quantity: 3,
+        },
+        {
+            price_data: {
+                currency: 'lkr',
+                product_data: {
+                    name: 'T-shirt - Red',
+                },
+                unit_amount: 20000,      // by default, amount is in cents
+            },
+            quantity: 2,
+        },
+    ];
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: items,
+        mode: 'payment',
+        success_url: `${process.env.CLIENT_URL}/payment/success`,   // direct when payment is successful
+        cancel_url: `${process.env.CLIENT_URL}/payment/failed`,     // direct when payment is cancelled / failed
+    });
+
+    res.status(200).json({
+        url: session.url,
+    });
 });
 
 
