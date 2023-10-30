6c0e6fc982a644d2ec6e456da041e147d936d4f0
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-10-30 15:42:36 +0530

    Restore userRoutes & userController

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,10 +13,10 @@ import { protect, permit } from "../middleware/authMiddleware.js";
 // '/api/users' is connected to this file
 // therefore no need to add '/api/users' in the routes
 router.post("/login", loginUser);
-router.post("/logout", protect, logoutUser);
-router.get("/details", protect, getUserAllDetails);
-// router.post("/logout", logoutUser);
-// router.get("/details", getUserAllDetails);
+// router.post("/logout", protect, logoutUser);
+// router.get("/details", protect, getUserAllDetails);
+router.post("/logout", logoutUser);
+router.get("/details", getUserAllDetails);
 
 // router.get("/total/count", protect, permit('Admin'), getTotalUserCount);
 router.get("/total/count", getTotalUserCount);
