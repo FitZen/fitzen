563c46bd2a9d1b2adcb5c09daf9ca1f5376d6b53
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-10-31 18:33:47 +0530

    Update trainerPackageController.js

--- backend/controllers/trainerPackageController.js
+++ backend/controllers/trainerPackageController.js
@@ -1,14 +1,14 @@
 import asyncHandler from 'express-async-handler';
-
 import {Stripe} from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SK);
 
+
 const checkoutTrainerPackage = asyncHandler(async (req, res) => {
-    console.log("req.body: ", req.body)
-    
-    const {title,sessions, price, trainerID, memberID} = req.body;
-    console.log("title: ", title,sessions, price, trainerID, memberID)
-    
+    const {title, sessions, price, trainerID, memberID} = req.body;
+
+    // console.log(title, sessions, price, trainerID, memberID);
+    //             Silver    15     15000  TR0001     VM0001
+
     const items = [
         {
             price_data: {
@@ -26,14 +26,21 @@ const checkoutTrainerPackage = asyncHandler(async (req, res) => {
     const session = await stripe.checkout.sessions.create({
         line_items: items,
         mode: 'payment',
-        success_url: `${process.env.CLIENT_URL}/member/instructors`,   // direct when payment is successful
-        cancel_url: `${process.env.CLIENT_URL}/payment/failed`,     // direct when payment is cancelled / failed
+        success_url: `${process.env.CLIENT_URL}/member/instructors`,    // direct when payment is successful
+        cancel_url: `${process.env.CLIENT_URL}/payment/failed`,         // direct when payment is cancelled / failed
     });
+
+    const sessionID = session.id;
+
+    if (sessionID) {
+
+    }
 
     res.status(200).json({
         url: session.url,
     });
 });
+
 
 export {
     checkoutTrainerPackage,
