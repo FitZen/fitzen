ca71c4238efa71a908a12d911df0d5a2b02ab72e
anjanasilva99 <108554413+anjanasilva99@users.noreply.github.com>
2023-10-31 11:25:06 +0530

    Create controller for chat

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -13,14 +13,26 @@ const createChat = async (req, res) => {
 };
 
 const userChats = async (req, res) => {
+
     try {
-        const chat = await ChatModel.find({
-        members: { $in: [req.params.userId] },
-    });
+        // console.log("original",req.params.userId);
+        // const id ="RT0001"
+        // console.log("id",id);
+        const id = req.params.userId;
+        // const stringWithQuotes = '"RT0001"';
+        const stringWithoutQuotes = id.slice(1, -1);
+
+        // const id = req.params.userId;
+        // const id ="RT0001"
+        // const chat = await ChatModel.find({members: { $in: [id] }});
+        const chat = await ChatModel.find({members: { $in: [stringWithoutQuotes] }});
+        // const chat = await ChatModel.find({members: { $in: [id] }});
+        // console.log(chat);
         res.status(200).json(chat);
     } catch (error) {
         res.status(500).json(error);
     }
+
 };
 
 const findChat = async (req, res) => {
