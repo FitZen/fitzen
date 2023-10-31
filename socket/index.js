8ffb5e6214d1e0b107dc09bfcac09a0b938663e7
anjanasilva99 <108554413+anjanasilva99@users.noreply.github.com>
2023-10-31 15:45:40 +0530

    Create index.js file

---
+++ socket/index.js
@@ -0,0 +1,29 @@
+const io = require("socket.io")(8800, { //import socket.io
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let activeUsers = [];
+
+io.on("connection", (socket) => {
+  // add new User
+  socket.on("new-user-add", (newUserId) => {
+    // if user is not added previously
+    if (!activeUsers.some((user) => user.userId === newUserId)) {
+      activeUsers.push({ userId: newUserId, socketId: socket.id });
+      console.log("New User Connected", activeUsers);
+    }
+    // send all active users to new user
+    // console.log("New User Connected", activeUsers);
+    io.emit("get-users", activeUsers);
+  })
+
+  socket.on("disconnect", () => {
+    // remove user from active users
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+    console.log("User Disconnected", activeUsers);
+    // send all active users to all users
+    io.emit("get-users", activeUsers);
+  });
+});
