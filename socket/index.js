c82e4ca9e7f7d69164ce4374ba77bcd5499cccc4
anjanasilva99 <108554413+anjanasilva99@users.noreply.github.com>
2023-11-01 04:15:53 +0530

    Add send-message socket

--- socket/index.js
+++ socket/index.js
@@ -19,6 +19,18 @@ io.on("connection", (socket) => {
     io.emit("get-users", activeUsers);
   })
 
+  // send message
+  socket.on("send-message", (data)=>{
+    const {receiverId} = data;
+    const user = activeUsers.find((user) => user.userId === receiverId);
+    console.log("Sending from socket to:", receiverId)
+    console.log("Data:", data)
+    if(user){
+      io.to(user.socketId).emit("receive-message", data);
+    }
+  })
+
+
   socket.on("disconnect", () => {
     // remove user from active users
     activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
