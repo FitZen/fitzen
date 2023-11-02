3efd80162e10e871bd587f6ec0504e6918f58c4f
Tharindu99 <91658088+tharindugunawardhana99@users.noreply.github.com>
2023-11-02 09:27:36 +0530

    Update trainerPackageRoutes.js

--- backend/routes/trainerPackageRoutes.js
+++ backend/routes/trainerPackageRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 const router = express.Router();
 import {
     getExistContracts,
-    checkoutTrainerPackage,
+    getExistContractMembers,
+    checkoutTrainerPackage
 } from "../controllers/trainerPackageController.js";
 
 
 router.get('/exist-contracts/:memberId', getExistContracts);
+router.get('/exist-contract-members/:trainerId', getExistContractMembers);
 router.post("/trainerPackageBuy", checkoutTrainerPackage);
 
 
